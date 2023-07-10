ae66bfa7225fb5b7e195737a876697903bcaa3b6
LxD5k <gustavo.dasilva_a@hotmail.com>
2023-07-10 20:14:55 -0300

    Carimbando com o logo da DIO

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData(){
-    const url = 'https://github.com/LxD5k/js-developer-portfolio/blob/25d97e8c7b36746cc386f9c8d5eea03f98180f5b/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+    const url = 'https://raw.githubusercontent.com/LxD5k/js-developer-portfolio/main/data/profile.json'
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,6 @@ function updateProfileInfo(profileData) {
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
     email.href = `mailto:${profileData.email}`
-
 }
 
 function updateSoftSkills(profileData) {
@@ -30,7 +29,7 @@ function updateSoftSkills(profileData) {
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title=${skill.name}"></li`).join('')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
 function updateLanguages(profileData) {
@@ -45,7 +44,8 @@ function updatePortfolio(profileData) {
             <li>
                 <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
                 <a href="${project.url}" target="_blank">${project.url}</a>
-            </li>`
+            </li>
+        `
     }).join('')
 }
 
@@ -57,14 +57,12 @@ function updateProfessionalExperience(profileData) {
                 <h3 class="title">${experience.name}</h3>
                 <p class="period">${experience.period}</p>
                 <p>${experience.description}</p>
-            </li>`
+            </li>
+        `
     }).join('')
 }
 
-
-
 (async () => {
-
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
