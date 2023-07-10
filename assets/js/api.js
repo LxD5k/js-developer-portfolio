4ec29700868dd18031bc417a40b943c5ddfe803b
LxD5k <gustavo.dasilva_a@hotmail.com>
2023-07-10 18:30:45 -0300

    Dinamizando as informações

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,5 @@
-function fetchProfileData(){
-    const url = ''
-    fetch()
+async function fetchProfileData(){
+    const url = 'https://github.com/LxD5k/js-developer-portfolio/blob/25d97e8c7b36746cc386f9c8d5eea03f98180f5b/data/profile.json'
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
